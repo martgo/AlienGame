61419b822df32a112445569ef8ef02ad7bccdfc7
martgo <martgo@op.pl>
2023-02-20 19:35:51 +0100

    adding movement of the ship

--- Alien.py
+++ Alien.py
@@ -18,6 +18,8 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
+            self._update_screen()
             #for event in pygame.event.get():
                 #if event.type == pygame.QUIT:
                     #sys.exit()
@@ -28,6 +30,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                    self.ship.rect.x+=1
             self.ship.blitme()
     def _update_screen(self):
         self.ship.blitme()
